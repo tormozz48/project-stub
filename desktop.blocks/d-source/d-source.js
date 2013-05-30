1e9357acb459b04e020045e8a0f6a73bd2f671a4
Bemer <bemer@yandex-team.ru>
2013-05-30 11:47:10 +0300

    Добавлен метод для построения ссылки для загрузки данных

--- desktop.blocks/d-source/d-source.js
+++ desktop.blocks/d-source/d-source.js
@@ -8,8 +8,61 @@ BEM.decl('d-source', {
     onSetMod : {
 
         'js' : function() {
-            console.log('data source inited');
+            console.log('-- data source initialized --');
+            console.log('- url : ' + this.params.url);
+            console.log('- order : ' + this.params.order);
+            console.log('- limit : ' + this.params.limit);
+
+            this._loadData();
         }
+
+    },
+
+    DEFAULT_URL: 'http://api-fotki.yandex.ru/api/top/',
+	DEFAULT_LIMIT: 50,
+
+	DATA_FORMAT: 'format=json',
+	CALLBACK: 'callback=?',
+
+	ORDERS: ['updated', 'rupdated', 'published', 'rpublished', 'created', 'rcreated'],
+	MIN_LIMIT: 0,
+	MAX_LIMIT: 100,
+
+    /**
+    * Метод для построения ссылки для загрузки данных с Яндекс фоток
+    **/
+	_createUrl : function() {
+        var url = null;
+
+        //добавляем url из параметров или берем url по умолчанию
+        url = this.params.url || this.DEFAULT_URL;
+        
+        //добавляем order из параметров или берем order по умолчанию
+        url += this.params.order ? this.params.order : this.ORDERS[0];
+        
+        url += '/';
+
+        //добавляем limit из парамеров или берем по умолчанию
+        // проводим дополнительные проверки на то, что limit положительный и меньше максимального лимита
+        var limit = (this.params.limit && this.params.limit > 0) ? this.params.limit : this.DEFAULT_LIMIT;
+        limit = this.params.limit <= this.MAX_LIMIT ? this.params.limit : this.MAX_LIMIT;
+        
+        url += '?limit=' + limit;
+        
+        //добавляем формат данных и callback для JSONP запроса
+        url += '&' + this.DATA_FORMAT + '&' + this.CALLBACK;
+        
+        console.log('-- url has been created --');
+        console.log('- url : ' + url);
+
+        return url;
+	},
+
+	_loadData : function() {
+        this._createUrl();
+	},
+
+    _parseData : function() {
 
     }
 
@@ -18,6 +71,12 @@ BEM.decl('d-source', {
 
 });
 
-BEM.create('d-source');
+//TODO подумать как сделать проброс параметров из страницы bemjson
+
+BEM.create('d-source', {
+    url: 'http://api-fotki.yandex.ru/api/top/',
+    order: 'updated',
+    limit: 50
+});
 
 })();
